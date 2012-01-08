6b0f974d4ca2ee0b94a45828f5ce3f374a6186ec
Mauro Stettler <mauro.stettler@gmail.com>
2012-01-08 16:12:19 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INSTALL_PATH="/usr/local/carbon_client"
+
+if [ -e ${INSTALL_PATH} ]
+then
+    echo "error, install path ${INSTALL_PATH} does already exist"
+    exit 1
+fi
+
+echo "preparing directory ${INSTALL_PATH}"
+mkdir ${INSTALL_PATH}
+
+cp ./carbon_server.conf ${INSTALL_PATH}
+cp ./generic_carbon_client.pl ${INSTALL_PATH}
+
+mkdir -p ${INSTALL_PATH}/plugins/execution_states
+
+chmod 777 ${INSTALL_PATH}/plugins/execution_states
+
+echo "done!"
+
+echo "TODO:"
+
+echo "- add ${INSTALL_PATH}/generic_carbon_client.pl to your crontab"
+echo "- make sure the user has write permissions on ${INSTALL_PATH}/plugins/execution_states"
